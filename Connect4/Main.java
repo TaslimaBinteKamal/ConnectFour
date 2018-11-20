dbec6a389645b76d6034d8f0afdf4243559e8954
TaslimaBinteKamal <44751387+TaslimaBinteKamal@users.noreply.github.com>
2018-11-20 19:41:07 +0600

    Add files via upload

---
+++ Connect4/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//GameModel gameModel = new GameModel(20);
+		
+		GameController gameController = new GameController(10);
+		
+	}
+
+}
